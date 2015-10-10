7a621db773cb8150d5879223c5f3480e22a325e2
gabo <gabo@debian>
2015-10-10 12:36:45 -0600

    openshif

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.set('port', server_port);
+app.set('ipaddr', server_ip_address);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,6 +19,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(server_port,function(){
+  console.log('listening in'+ server_port +' ...');
 });
